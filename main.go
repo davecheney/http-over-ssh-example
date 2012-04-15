a8ca13c46556ccb0dc938c218c2bfc17644e13c5
Dave Cheney <dave@cheney.net>
2012-04-15 15:11:50 +1000

    initial commit

---
+++ main.go
@@ -0,0 +1,50 @@
+// HTTP serving using your ssh server as a 
+// frontend proxy -- it's 100% wacky
+package main
+
+import (
+	"code.google.com/p/go.crypto/ssh"
+	"flag"
+	"log"
+	"net/http"
+)
+
+var (
+        sshuser    = flag.String("ssh.user", "", "ssh username")
+        sshpass    = flag.String("ssh.pass", "", "ssh password")
+)
+
+// password implements the ClientPassword interface
+type password string
+
+func (p password) Password(user string) (string, error) {
+        return string(p), nil
+}
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	config := &ssh.ClientConfig{
+                User: *sshuser,
+                Auth: []ssh.ClientAuth{
+                        ssh.ClientAuthPassword(password(*sshpass)),
+                },
+        }
+        conn, err := ssh.Dial("tcp", "localhost:22", config)
+        if err != nil {
+                log.Fatalf("unable to connect: %s", err)
+        }
+        defer conn.Close()
+
+	l, err := conn.Listen("tcp", "0.0.0.0:8080")
+	if err != nil {
+		log.Fatalf("unable to register tcp forward: %v", err)
+	}
+	defer l.Close()
+
+	s := new(http.Server)
+	log.Printf("http server done: %v", s.Serve(l))
+	
+}
